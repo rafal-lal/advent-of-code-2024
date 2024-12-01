2ec36277295273ee9076e37dd219d9094da4ba10
Rafal Lal <rafal.lal.rl.rl@gmail.com>
2024-12-01 16:18:15 +0100

    Add day 1 solutions
    Signed-off-by: Rafal Lal <rafal.lal.rl.rl@gmail.com>

---
+++ day1/day1.cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <iostream>
+#include <optional>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../utils/utils.hpp"
+
+void star1() {
+    auto lines = Utils::parseFile("input.txt");
+    if (!lines) {
+        exit(1);
+    }
+
+    std::vector<u_int32_t> left, right;
+    for (const auto& line : *lines) {
+        u_int32_t leftNum, rightNum;
+
+        std::istringstream iss(line);
+        if (iss >> leftNum >> rightNum) {
+            left.push_back(leftNum);
+            right.push_back(rightNum);
+        } else {
+            exit(1);
+        }
+    }
+
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    u_int32_t diff = 0;
+    for (size_t i = 0; i < left.size(); ++i) {
+        diff += abs(left[i] - right[i]);
+    }
+    std::cout << "Distance: " << diff << "\n";
+
+    return;
+}
+
+void star2() {
+    auto lines = Utils::parseFile("input.txt");
+    if (!lines) {
+        exit(1);
+    }
+
+    std::vector<u_int32_t> left, right;
+    for (const auto& line : *lines) {
+        u_int32_t leftNum, rightNum;
+
+        std::istringstream iss(line);
+        if (iss >> leftNum >> rightNum) {
+            left.push_back(leftNum);
+            right.push_back(rightNum);
+        } else {
+            exit(1);
+        }
+    }
+
+    u_int32_t similarity = 0;
+    for (const auto& leftNum : left) {
+        u_int32_t occurrences = 0;
+        for (const auto& rightNum : right)
+            if (leftNum == rightNum) occurrences++;
+        similarity += leftNum * occurrences;
+    }
+    std::cout << "Similarity: " << similarity << "\n";
+
+    return;
+}
+
+int main(int argc, char const* argv[]) {
+    star1();
+    star2();
+
+    return 0;
+}
---
+++ utils/utils.hpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace Utils {
+std::optional<std::vector<std::string>> parseFile(const std::string &);
+}
+
+std::optional<std::vector<std::string>> Utils::parseFile(
+    const std::string &filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "error: could not open file " << filename << "\n";
+        return std::nullopt;
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) lines.push_back(line);
+
+    return lines;
+}
