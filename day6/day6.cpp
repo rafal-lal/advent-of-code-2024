bf44edc66a882f19046958bf8b4e2b15646cff52
Rafal Lal <rafal.lal.rl.rl@gmail.com>
2024-12-11 12:50:06 +0100

    Add day6 star 2 solution
    Signed-off-by: Rafal Lal <rafal.lal.rl.rl@gmail.com>

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -1,3 +1,6 @@
+#include <unordered_map>
+#include <utility>
+
 #include "../utils/utils.hpp"
 
 void star1(std::vector<std::string> &lines) {
@@ -51,15 +54,14 @@ void star2(std::vector<std::string> &lines) {
         }
         floor.push_back(tiles);
     }
+    int startI = currI, startJ = currJ;
+    auto startingFloor(floor);
 
-    int sum = 0;
     auto currDir = utils::Direction::N;
     while (utils::checkDirection<std::string>(
         floor, currI, currJ, currDir, [&](std::string tile, int nextI, int nextJ) -> bool {
-            // utils::print2DVector<std::string>(floor);
             if (floor[currI][currJ] != "X") {
                 floor[currI][currJ] = "X";
-                sum++;
             }
             if (tile != "#") {
                 currI = nextI;
@@ -70,8 +72,62 @@ void star2(std::vector<std::string> &lines) {
 
             return true;
         }));
+    floor[currI][currJ] = "X";
+    // utils::print2DVector(floor);
 
-    std::cout << "Sum 2: " << sum - 1 + 2 << "\n";
+    std::vector<std::vector<std::vector<std::string>>> floors;
+    for (size_t i = 0; i < floor.size(); i++) {
+        for (size_t j = 0; j < floor[i].size(); j++) {
+            if (i == startI && j == startJ) {
+                continue;
+            }
+            if (floor[i][j] == "X") {
+                std::vector<std::vector<std::string>> copy(startingFloor);
+                copy[i][j] = "#";
+                floors.push_back(copy);
+            }
+        }
+    }
+
+    int sum = 0;
+    float cnt = 0;
+    for (auto &fl : floors) {
+        cnt++;
+        std::cout << "Progress: " << float(cnt / floors.size()) * 100 << "%\n";
+        currI = startI;
+        currJ = startJ;
+        currDir = utils::Direction::N;
+        bool directionChange = false;
+        std::unordered_map<std::pair<int, int>, int, utils::PairHash> positions;
+        std::vector<std::pair<int, int>> visited;
+        while (utils::checkDirection<std::string>(
+            fl, currI, currJ, currDir, [&](std::string tile, int nextI, int nextJ) -> bool {
+                if (!directionChange) {
+                    positions[std::make_pair(currI, currJ)]++;
+                    if (positions[std::make_pair(currI, currJ)] > 4) {
+                        sum++;
+                        return false;
+                    }
+                }
+
+                if (fl[currI][currJ] != "X") {
+                    fl[currI][currJ] = "X";
+                }
+
+                if (tile != "#") {
+                    currI = nextI;
+                    currJ = nextJ;
+                    directionChange = false;
+                    return true;
+                }
+                currDir = static_cast<utils::Direction>((static_cast<int>(currDir) + 2) % 8);
+                directionChange = true;
+
+                return true;
+            }));
+    }
+
+    std::cout << "Sum 2: " << sum << "\n";
 }
 
 int main(int argc, char const *argv[]) {
--- utils/utils.hpp
+++ utils/utils.hpp
@@ -111,4 +111,13 @@ bool isIn2DGrid(const std::vector<std::vector<T>>& grid, int i, int j) {
     return true;
 }
 
+struct PairHash {
+    template <typename T1, typename T2>
+    std::size_t operator()(const std::pair<T1, T2>& p) const {
+        std::size_t h1 = std::hash<T1>()(p.first);
+        std::size_t h2 = std::hash<T2>()(p.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 }  // namespace utils
